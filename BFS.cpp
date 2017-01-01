c4c2516741007997206a896ad1cabe5797168566
Arvind Pandey <arvindpandey106@gmail.com>
2017-01-01 21:10:52 +0530

    BFS code

---
+++ BFS.cpp
@@ -0,0 +1,91 @@
+/*
+Author :  ARVIND PANDEY
+ */
+#include <iostream>
+#include <stdio.h>
+#include <climits>
+#include <cstring>
+#include <algorithm>
+#include <string.h>
+#include <stdlib.h>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+
+using namespace std;
+struct _ {
+    ios_base::Init i;
+
+    _() {
+        cin.sync_with_stdio(0);
+        cin.tie(0);
+    }
+} _;
+
+#define FOR(i,a,b) for(i=a;i<b;i++)
+#define pb(x) push_back(x)
+#define ll long long
+#define mp make_pair
+
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef vector<pii> vii;
+typedef vector<vii> vvii;
+
+ll power(ll a, ll n, ll p) //////             O(log n)
+{
+    ll ans = 1;
+    while (n) {
+        if (n % 2 == 1)
+            ans =(ans * a) % p;
+        a = (a * a) % p;
+        n /= 2;
+    }
+    return ans % p;
+}
+
+ll gcd(ll a, ll b) ////                 O(log(a+b))
+{
+    if (b==0)
+        return a;
+    return gcd(b,a%b);
+}
+////////////////////////////////////////////////////////////////////////////////////CODE BEGINS/////////////////////////////////////////////////////////
+
+//denote tree using vector of vectors
+vector<int> tree[10];
+//we will use queue for implementing BFS
+queue<int> Q;
+void bfs() {
+    int i;
+    while(!Q.empty()) {
+        int node = Q.front();
+        // print the current node
+        cout<<node<<"\n";
+        // pop from queue
+        Q.pop();
+        // traverse over the child of the current node and call bfs() on its child
+        FOR(i,0,tree[node].size()) {
+            Q.push(tree[node][i]);
+        }
+    }
+}
+void constructSampleTree() {
+    tree[1].pb(2);
+    tree[1].pb(3);
+    tree[2].pb(4);
+    tree[2].pb(5);
+    tree[3].pb(6);
+    tree[3].pb(7);
+}
+
+int main() {
+    constructSampleTree();
+    //push root node to queue
+    Q.push(1);
+    // pass the root node to bfs()
+    bfs();
+}
